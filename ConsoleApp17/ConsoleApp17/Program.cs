1fe05865a5bb78d1a2b952177a836158fe9c2a20
vlad gryaznov <grvl05@gmail.com>
2019-04-21 00:06:15 +0300

    visual studio projects

---
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lets start");
+            Console.ReadKey();
+        }
+
+    }
+}
